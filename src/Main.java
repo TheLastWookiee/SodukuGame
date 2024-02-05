74c90fc275c5d7ee025a0f82300198b92e9210ff
Caiden Henn <caidensky@gmail.com>
2024-02-05 14:39:16 -0500

    Finished implementing the sudoku functionality

--- src/Main.java
+++ src/Main.java
@@ -1,89 +1,118 @@
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 
-
+/**
+ * A soduku game programed in Java by Caiden Henn
+ */
 import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-
-Soduku.display();
-    System.out.println("Continue playing(Y/N)?");
+        //loops if there is an invalid input
+        boolean noloop=false;
+        Soduku.display();
+        while(!noloop){
+        System.out.println("Continue playing(Y/N)?");
         String userInput = in.nextLine();
 
-        if(userInput.equals("N")){
+        if (userInput.equals("N")) {
             System.exit(0);
-        } else if(userInput.equals("Y")) {
-            getInput(in);
+        } else if (userInput.equals("Y")) {
+            getInput(in, userInput);
+            noloop=true;
+        }else{
+            System.out.println("Invalid Input");
         }
+    }}
 
-        private static void getInput(Scanner in){
+    static private void getInput (Scanner in, String userInput){
             boolean valid = false;
-
-            while (valid = false) {
+            //main loop
+            while (!Soduku.isSolved()) {
+                //get data from user
                 System.out.println("Enter Row:");
                 int row = Integer.parseInt(in.nextLine());
                 System.out.println("Enter Column:");
                 int col = Integer.parseInt(in.nextLine());
                 System.out.println("Enter Value:");
                 int val = Integer.parseInt(in.nextLine());
-                if (!Soduku.isValueValid(row) || !Soduku.isValueValid(col) || !Soduku.isValueValid(val)) {
-                System.out.println("Invalid Input");
+                //error message for invalid row/col
+                if (!Soduku.isValueValid(row) || !Soduku.isValueValid(col)) {
+                    System.out.println("Invalid Row/Column");
+                    boolean noloop=false;
+                    while(!noloop){
                     System.out.println("Continue playing(Y/N)?");
-                    ;
+                    String userInput1 = in.nextLine();
 
-                    if(userInput.equals("N")){
+                    if (userInput1.equals("N")) {
                         System.exit(0);
-                    } else if(userInput.equals("Y")) {
+                    } else if (userInput1.equals("Y")) {
+                        noloop=true;
+                    }else {
+                        System.out.println("Invalid Input");
+
+                    }
+                }
+                    //error message for invalid value
+                }else if(!Soduku.isValueValid(val)){
+                    System.out.println("Invalid Value");
+                    System.out.println("Continue playing(Y/N)?");
+                    String userInput1 = in.nextLine();
+
+                    if (userInput1.equals("N")) {
+                        System.exit(0);
+                    } else if (userInput1.equals("Y")) {
                         valid = false;
                     }
-                }else{break;}
+                }
+
+
+
+                else {
+                    //if value is correct, prints and displays
+                    if(Soduku.isRowColumnValid(row,col)){
+                        if(Soduku.isEntryCorrect(val,row,col)){
+
+                                Soduku.sudokuBoard[row-1][col-1] = val;
+                                System.out.println("Correct!");
+                                Soduku.display();
+
+
+                        }else{//if value is not correct, prints and displays
+                            System.out.println("NOT Correct!");
+                            Soduku.display();
+                            //Continue Playiyng?
+                            System.out.println("Continue playing(Y/N)?");
+
+                            String userInput1 = in.nextLine();
+
+                            if (userInput1.equals("N")) {
+                                System.exit(0);
+                            } else if (userInput1.equals("Y")) {
+                                ;
+                            }
+                        }
+
+                    }else{//Not a valid entry, stops and asks if you want to keep playing
+                        System.out.println("Wrong Entry");
+                        Soduku.display();
+                        System.out.println("Continue playing(Y/N)?");
+
+                        String userInput1 = in.nextLine();
+
+                        if (userInput1.equals("N")) {
+                            System.exit(0);
+                        } else if (userInput1.equals("Y")) {
+                        ;
+                        }
+
+                    }
+
+                }
 
             }
+            System.out.println("Congratulations, you solved it!");
         }
-
-
-
-
-
-        int row, column, value;
-        while(1!=0){
-        System.out.println("Enter Row:");
-
-        try {
-            int intValue = Integer.parseInt(in.nextLine());
-            if(intValue < 1 || intValue >9){
-               Operations.InvalidInput();
-            }else{break;}
-        } catch (NumberFormatException e) {
-            // Handle the case where parsing as an integer fails
-            Operations.InvalidInput();
-        }}
-        while(1!=0){
-        System.out.println("Enter Column:");
-
-        try {
-            int intValue = Integer.parseInt(in.nextLine());
-            if(intValue < 1 || intValue >9){
-                Operations.InvalidInput();
-            }else{break;}
-
-        } catch (NumberFormatException e) {
-            // Handle the case where parsing as an integer fails
-            Operations.InvalidInput();
-        }}
-        while(1!=0){
-        System.out.println("Enter Value:");
-
-        try {
-            int intValue = Integer.parseInt(in.nextLine());
-            if(intValue < 1 || intValue >9){
-                Operations.InvalidInput();
-            }else{break;}
-        } catch (NumberFormatException e) {
-            // Handle the case where parsing as an integer fails
-            Operations.InvalidInput();
-        }}
     }
 
-}
+
+
--- src/Soduku.java
+++ src/Soduku.java
@@ -14,8 +14,28 @@ public class Soduku {
             {2, 8, 7, 4, 1, 9, 6, 0, 5},
             {3, 4, 5, 2, 8, 6, 1, 7, 9}
     };
-
+//Original
+    /*{5, 3, 4, 0, 7, 8, 9, 1, 2},
+            {6, 7, 0, 1, 9, 5, 3, 4, 8},
+            {1, 9, 8, 3, 4, 2, 5, 0, 7},
+            {8, 0, 9, 7, 6, 1, 4, 2, 3},
+            {4, 2, 6, 8, 0, 3, 7, 9, 0},
+            {7, 1, 3, 9, 2, 4, 8, 5, 6},
+            {9, 0, 1, 5, 3, 7, 2, 8, 4},
+            {2, 8, 7, 4, 1, 9, 6, 0, 5},
+            {3, 4, 5, 2, 8, 6, 1, 7, 9}*/
+    //Solved
+    /*{5, 3, 4, 6, 7, 8, 9, 1, 2},
+            {6, 7, 2, 1, 9, 5, 3, 4, 8},
+            {1, 9, 8, 3, 4, 2, 5, 6, 7},
+            {8, 5, 9, 7, 6, 1, 4, 2, 3},
+            {4, 2, 6, 8, 5, 3, 7, 9, 1},
+            {7, 1, 3, 9, 2, 4, 8, 5, 6},
+            {9, 6, 1, 5, 3, 7, 2, 8, 4},
+            {2, 8, 7, 4, 1, 9, 6, 3, 5},
+            {3, 4, 5, 2, 8, 6, 1, 7, 9}*/
     static void display() {
+        //runs a for loop through the columns and rows and displays the array sudokuBoard
         for (int i = 0; i < 9; i++) {
             for (int j = 0; j < 9; j++) {
                 System.out.print(sudokuBoard[i][j] + " ");
@@ -32,36 +52,35 @@ public class Soduku {
     }
 
     //I have done the validSoduku leetcode problem before so I pretty much did the same thing here, there can not be multiple integers in a set so this is a easy way of checking for duplicates
-    boolean isSolved() {
-        HashSet<Integer> row = new HashSet<>();
-        HashSet<Integer> col = new HashSet<>();
-        HashSet<Integer> block = new HashSet<>();
-        for (int i = 0; i < 9; i++) {
-            for (int j = 0; j < 9; j++) {
-                int num = sudokuBoard[i][j];
-                if (num != 0) {
-                    if (!row.add(sudokuBoard[i][j]) || !col.add(sudokuBoard[i][j])) {
-                        return false;
-                    }
-                    if (!block.add(sudokuBoard[i / 3 * 3 + j / 3][i % 3 * 3 + j % 3])) {
+    static boolean isSolved() {
+        //this function stores hash sets as integers with a unique string for the rows, columns, and block values which makes it so that there are no repeats.
+        Set seen = new HashSet<>();
+
+        for (int i = 0; i < 9; ++i) {
+            for (int j = 0; j < 9; ++j) {
+                int number = sudokuBoard[i][j];
+                //the block code works by specifying the area/3 of the columns and rows
+                    if (!seen.add(number + " in row " + i) || !seen.add(number + " in column " + j) || !seen.add(number + " in block " + i / 3 + "-" + j / 3) || number ==0) {
                         return false;
                     }
                 }
             }
-        }
+
+
         return true;
-
     }
-
+//just a basic method that checks that the value is between 1 and 9
     static boolean isValueValid(int num) {
         if (num <= 9 && num >= 1) {
             return true;
 
         } else return false;
     }
-        boolean isRowColumnValid(int row, int col, int[][] sod){
+    //checks that there is a 0 where the rows and columns are specified.
+    //the values are subtracted by 1 to make it so it lines up with user input data
+        static boolean isRowColumnValid(int row, int col){
             if (row <= 9 && row >= 1 && col <= 9 && col >= 1) {
-                if(sod[row][col] == 0){
+                if(sudokuBoard[row-1][col-1] == 0){
                     return true;
 
                 }
@@ -69,26 +88,30 @@ public class Soduku {
             }
             return false;
         }
-        boolean isEntryCorrect(int num, int[][] sod, int row, int col){
+        //checks if entry is correct. It checks if there are duplicates first in the rows and the columns.
+        static boolean isEntryCorrect(int num, int row, int col){
             for (int j = 0; j < 9; j++) {
-                if (sod[row][j] == num) {
+                if (sudokuBoard[row-1][j] == num) {
+
                     return false; // Number already exists in the row
                 }
             }
 
             // Check the column
             for (int i = 0; i < 9; i++) {
-                if (sod[i][col] == num) {
+                if (sudokuBoard[i][col-1] == num) {
+
                     return false; // Number already exists in the column
                 }
             }
 
             // Check the 3x3 block
-            int blockStartRow = (row / 3) * 3;
-            int blockStartCol = (col / 3) * 3;
+            int blockStartRow = ((row - 1) / 3) * 3;
+            int blockStartCol = ((col - 1) / 3) * 3;
             for (int i = blockStartRow; i < blockStartRow + 3; i++) {
                 for (int j = blockStartCol; j < blockStartCol + 3; j++) {
-                    if (sod[i][j] == num) {
+                    if (sudokuBoard[i][j] == num) {
+
                         return false; // Number already exists in the block
                     }
                 }
@@ -96,6 +119,8 @@ public class Soduku {
 
             return true; // Entry is correct
         }
+
+
 
         }
 
